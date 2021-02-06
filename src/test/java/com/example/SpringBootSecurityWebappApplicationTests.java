e3f0cea06fb09adf8f907216a1407b420a5f3bd9
Bakwanyana Thobela <bmthobela@gmail.com>
2021-02-06 22:54:26 +0200

    correct package

--- src/test/java/com/example/SpringBootSecurityWebappApplicationTests.java
+++ src/test/java/com/example/SpringBootSecurityWebappApplicationTests.java
@@ -1,7 +1,9 @@
-package com.modjadji.springbootsecuritywebapp;
+package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class SpringBootSecurityWebappApplicationTests {
