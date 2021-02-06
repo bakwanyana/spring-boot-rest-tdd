0fa391a60a67a7ff2c77d9ad076c4e0579bb0ecd
Bakwanyana Thobela <bmthobela@gmail.com>
2021-02-06 22:11:47 +0200

    initial commit

---
+++ src/test/java/com/modjadji/springbootsecuritywebapp/SpringBootSecurityWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modjadji.springbootsecuritywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
