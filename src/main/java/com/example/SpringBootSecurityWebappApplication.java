41efc3957a8613d1a6f55dd02598f05d0d784fbd
Bakwanyana Thobela <bmthobela@gmail.com>
2021-02-07 02:43:23 +0200

    added swagger ui

--- src/main/java/com/example/SpringBootSecurityWebappApplication.java
+++ src/main/java/com/example/SpringBootSecurityWebappApplication.java
@@ -2,12 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootSecurityWebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityWebappApplication.class, args);
 	}
-
 }
